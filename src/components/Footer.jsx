60fdb798131a2c81665015054c919f12b30cfd1a
Patricio Salazar <psebastiansalazar@gmail.com>
2025-05-17 15:42:59 -0700

    wrapped footer text in small element and updated text colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,20 @@ import React from "react";
 function Footer({ isDetailsOpen }) {
   return (
     <footer
-      className={`text-center text-stone-200 dark:text-stone-700 ${
+      className={`text-center ${
         isDetailsOpen ? "static" : "absolute"
       } bottom-2 left-0 right-0`}
     >
-      copyright 2024 Doubles by{" "}
-      <a
-        href='https://patriciosalazar.dev'
-        target='_blank'
-        className='text-stone-200 dark:text-stone-700 underline hover:text-stone-400'
-      >
-        Patricio Salazar
-      </a>
+      <small>
+        copyright 2024-present Doubles by{" "}
+        <a
+          href='https://patriciosalazar.dev'
+          target='_blank'
+          className='underline text-inherit hover:text-blue-500'
+        >
+          Patricio Salazar
+        </a>
+      </small>
     </footer>
   );
 }
