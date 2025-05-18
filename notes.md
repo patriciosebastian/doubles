977deb2c24e0ab7528f7c3a42aa10df3281706ee
Patricio Salazar <psebastiansalazar@gmail.com>
2025-05-17 19:26:54 -0700

    Revert "componentized App.jsx"
    This reverts commit 5bc6334eeff37d72d9d54fdbc981fe646d581612.

---
+++ notes.md
@@ -0,0 +1,2 @@
+NOTE: Endless mode (no timer)
+NOTE: Display 'Timed Out' for the users response when the timer beats the user before they submit a response
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import { useState, useEffect, useRef } from 'react';
-import SettingsMenu from "./components/SettingsMenu";
-import TimerDisplay from "./components/TimerDisplay";
-import GameOverScreen from "./components/GameOverScreen";
-import GameInputForm from "./components/GameInputForm";
-import Footer from "./components/Footer";
 
 function App() {
   const [number, setNumber] = useState(null);
@@ -303,79 +298,140 @@ function App() {
 
   return (
     <>
-      <div className='relative'>
-        <h1 className='text-4xl sm:text-5xl md:text-6xl font-bold text-center mt-4 mb-6'>
-          Doubles
-        </h1>
-        <div className='ml-auto absolute right-0 top-[0.85rem] md:top-6'>
-          <button
-            onClick={toggleSettingsVisibility}
-            className='text-gray-600 hover:text-gray-900 bg-transparent'
-            ref={gearIconRef}
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth='1.5'
-              stroke='currentColor'
-              className='w-6 h-6'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z'
-              />
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
-              />
+      <div className="relative">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-center mt-4 mb-6">Doubles</h1>
+        <div className="ml-auto absolute right-0 top-[0.85rem] md:top-6">
+          <button onClick={toggleSettingsVisibility} className="text-gray-600 hover:text-gray-900 bg-transparent" ref={gearIconRef}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             </svg>
           </button>
-          <SettingsMenu
-            settingsVisible={settingsVisible}
-            settingsRef={settingsRef}
-            customStart={customStart}
-            handleCustomStartChange={handleCustomStartChange}
-            handleCustomStartSubmit={handleCustomStartSubmit}
-            clearCustomStart={clearCustomStart}
-            mode={mode}
-            handleModeChange={handleModeChange}
-          />
+    
+          {settingsVisible && (
+            <div className="mt-5 bg-gray-200 p-6 rounded absolute right-0 shadow-lg w-64 lg:w-[23rem]" ref={settingsRef}>
+              <label className="block text-lg md:text-xl dark:text-[#242424]">Choose a fixed starting number:</label>
+              <input
+                type="number"
+                value={customStart}
+                onChange={handleCustomStartChange}
+                className="border border-black p-2 rounded mt-2 w-full"
+                placeholder="Enter a starting number"
+              />
+              <button
+                onClick={handleCustomStartSubmit}
+                className="bg-blue-500 text-white px-4 py-2 rounded mt-2 w-full"
+              >
+                Update Number
+              </button>
+              <button
+                onClick={clearCustomStart}
+                className="bg-gray-500 text-white px-4 py-2 rounded mt-2 w-full"
+              >
+                Clear Starting Number
+              </button>
+              <div className="timer-modes mt-4">
+                <label className="block text-lg md:text-xl dark:text-[#242424]">Timer Mode:</label>
+                <div className="dark:text-[#242424]">
+                  <label className="block">
+                    <input
+                      type="radio"
+                      value="easy"
+                      checked={mode === 'easy'}
+                      onChange={() => handleModeChange('easy')}
+                      className="mr-2"
+                    />
+                    Easy (10s ⏱️⏱️⏱️)
+                  </label>
+                  <label className="block">
+                    <input
+                      type="radio"
+                      value="medium"
+                      checked={mode === 'medium'}
+                      onChange={() => handleModeChange('medium')}
+                      className="mr-2"
+                    />
+                    Medium (8s ⏱️⏱️)
+                  </label>
+                  <label className="block">
+                    <input
+                      type="radio"
+                      value="hard"
+                      checked={mode === 'hard'}
+                      onChange={() => handleModeChange('hard')}
+                      className="mr-2"
+                    />
+                    Hard (6s ⏱️- Default)
+                  </label>
+                  <label className="block">
+                    <input
+                      type="radio"
+                      value="evil"
+                      checked={mode === 'evil'}
+                      onChange={() => handleModeChange('evil')}
+                      className="mr-2"
+                    />
+                    Evil (4s 👿)
+                  </label>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      <div className='container mx-auto p-4 text-center'>
-        <TimerDisplay timer={timer} getTimeColor={getTimeColor} />
-        <p className='mb-24 text-gray-400 text-base sm:text-lg'>Start typing</p>
+      <div className="container mx-auto p-4 text-center">
+        <p className={`text-5xl sm:text-6xl text-${getTimeColor()}`}>{timer}s</p>
+        <p className="mb-24 text-gray-400 text-base sm:text-lg">Start typing</p>
+
         {gameOver ? (
-          <GameOverScreen
-            doublesCount={doublesCount}
-            gameModeAtEnd={gameModeAtEnd}
-            correctAnswer={correctAnswer}
-            highScore={highScore}
-            answers={answers}
-            formatNumber={formatNumber}
-            startNewGame={startNewGame}
-            handleShareScore={handleShareScore}
-            setIsDetailsOpen={setIsDetailsOpen}
-            detailsRef={detailsRef}
-          />
+          <div className="text-center">
+            <p className="text-red-500 text-lg sm:text-xl lg:text-2xl mb-14">Game Over! You doubled <span className="font-bold">{doublesCount}</span> times on <span className="font-bold">{gameModeAtEnd}</span></p>
+            <p className="text-lg sm:text-xl">The correct answer was:<br />
+              <span className="font-bold">{formatNumber(correctAnswer)}</span>
+            </p>
+            <p className="text-lg sm:text-xl mb-2">Your highest score: {highScore}</p>
+            <details className="mb-20" onToggle={(e) => setIsDetailsOpen(e.target.open)} ref={detailsRef}>
+              <summary className="hover:cursor-pointer">Your Replay</summary>
+              <small className="text-stone-400 dark:text-stone-600">you can toggle this dropdown with the space bar</small>
+              <div className="border rounded bg-gray-100 dark:bg-stone-700 dark:border-transparent text-black dark:text-white">
+                <div className="grid grid-rows-6 grid-flow-col gap-y-4">
+                  {answers.map((answer, index) => (
+                    <div key={index} className="p-4 border-b border-gray-300 dark:border-gray-500">
+                      {answer.isCorrect ? `✅ ${formatNumber(answer.value)}` : `❌ ${formatNumber(answer.value)}`}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </details>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded mr-4 mt-4 sm:mt-0" onClick={startNewGame}>
+              Start New Game
+            </button>
+            <button className="bg-green-500 text-white px-4 py-2 rounded mt-4 sm:mt-0" onClick={handleShareScore}>
+              Share Your Score
+            </button>
+            <p className="text-left pl-20 w-fit text-stone-400 hidden md:block">Or Press Enter &#8629;</p>
+          </div>
         ) : (
-          <GameInputForm
-            number={number}
-            inputValue={inputValue}
-            handleInputChange={handleInputChange}
-            handleSubmit={handleSubmit}
-            inputRef={inputRef}
-            formatNumber={formatNumber}
-            doublesCount={doublesCount}
-            startNewGame={startNewGame}
-            getTimeColor={getTimeColor}
-          />
+          <div className="text-center">
+            <p className="text-lg sm:text-2xl inline mr-4">Current Number: <span className="font-semibold">{formatNumber(number)}</span></p>
+            <button className="bg-transparent mb-4 text-sm" onClick={startNewGame}>New Number</button>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="number"
+                value={inputValue}
+                onChange={handleInputChange}
+                className={`border border-black outline-${getTimeColor()} focus:outline-${getTimeColor()} p-4 rounded text-xl sm:text-3xl w-full`}
+                placeholder="Enter your answer"
+                ref={inputRef}
+              />
+            </form>
+            <p className="mt-4 text-lg sm:text-xl">Streak: {doublesCount}</p>
+          </div>
         )}
       </div>
-      <Footer isDetailsOpen={isDetailsOpen} />
+
+      {/* Footer */}
+      <footer className={`text-center text-stone-200 dark:text-stone-700 ${isDetailsOpen ? 'static' : 'absolute'} bottom-2 left-0 right-0`}>copyright 2024 Doubles by <a href="https://patriciosalazar.dev" target="_blank" className="text-stone-200 dark:text-stone-700 underline hover:text-stone-400">Patricio Salazar</a></footer>
     </>
   );
 }
